b3a071267212845a53c037dd1d5e9ff592e8bc3e
LuisTab22 <119250287+LuisTab22@users.noreply.github.com>
2022-12-10 08:21:00 -0500

    Add files via upload

---
+++ Menú.py
@@ -0,0 +1,12 @@
+# -*- coding: iso-8859-15 -*-
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	print "Menú del día"
+	print "Seleccionar entre los siguientes productos:"
+	print "Lomo saltado........ S/.15"
+	print "Tallarines rojos.... S/.13"
+
